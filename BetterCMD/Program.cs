56a73200f117db643ea2f77ba58ae669d389e0f0
Thijs <thijs@licht51.de>
2020-06-13 20:32:48 +0200

    Created Solution

---
+++ BetterCMD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BetterCMD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
