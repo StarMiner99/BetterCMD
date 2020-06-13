5fcb88a44c17af2d53dc2e924f5ef7d9ba157a28
Thijs <thijs@licht51.de>
2020-06-13 22:33:26 +0200

    added prompt color and special chars

--- BetterCMD/BetterCMD.cs
+++ BetterCMD/BetterCMD.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.Drawing;
 
 namespace BetterCMD
 {
     class BetterCMD
     {
+        static string promptFormat = Environment.GetEnvironmentVariable("CMDPrompt");
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (promptFormat == null)
+            {
+                Console.WriteLine("Configuring BetterCMD, Please Wait...");
+                Environment.SetEnvironmentVariable("CMDPrompt", "#14 BetterCMD #1 *10 nice~#1 test~~");
+                promptFormat = Environment.GetEnvironmentVariable("CMDPrompt");
+            }
+            Prompt prompt = new Prompt(promptFormat);
+            prompt.printPrompt();
         }
     }
 }
---
+++ BetterCMD/Prompt.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BetterCMD
+{
+    public class Prompt
+    {
+        private string promptFormat;
+        public Prompt(string promptFormat)
+        {
+            this.promptFormat = promptFormat;
+        }
+
+        public void printPrompt ()
+        {
+            /* example
+                test #14 yellow{hostname} *9 {user} \*>
+            */
+            
+            //check for first special char
+            int nextFg = promptFormat.IndexOf("#");
+            int nextBg = promptFormat.IndexOf("*");
+            int nextSc = promptFormat.IndexOf("\\");
+            int nextSb = promptFormat.IndexOf("{");
+            int nextSe = promptFormat.IndexOf("}");
+            int[] special = new int[5] {nextFg, nextBg, nextSc, nextSb, nextSe};
+
+            for (int i = 0; i < promptFormat.Length; i++)
+            {
+                if (promptFormat[i] == '#')
+                {
+                    int nextWhiteSpace = promptFormat.IndexOf(" ", i);
+                    int colorCode = Int32.Parse(promptFormat.Substring(i+1, nextWhiteSpace - i));
+                    Console.ForegroundColor = (ConsoleColor)colorCode;
+                    i += promptFormat.Substring(i, nextWhiteSpace - i).Length;
+                } else if (promptFormat[i] == '*')
+                {
+                    int nextWhiteSpace = promptFormat.IndexOf(" ", i);
+                    int colorCode = Int32.Parse(promptFormat.Substring(i+1, nextWhiteSpace - i));
+                    Console.BackgroundColor = (ConsoleColor)colorCode;
+                    i += promptFormat.Substring(i, nextWhiteSpace - i).Length;
+                } else if (promptFormat[i] == '~')
+                {
+                    Console.Write(promptFormat[++i]);
+                } else if (promptFormat[i] == '{')
+                {
+                    
+                }
+                else
+                {
+                    Console.Write(promptFormat[i]);
+                }
+            }
+        }
+    }
+}
