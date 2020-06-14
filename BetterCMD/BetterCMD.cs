bf81db1fdf0be6d578e3d68a3ecbf0a7f9e81a06
Thijs <thijs@licht51.de>
2020-06-14 09:12:09 +0200

    prompt variables

--- BetterCMD/BetterCMD.cs
+++ BetterCMD/BetterCMD.cs
@@ -1,21 +1,28 @@
 ﻿using System;
-using System.Drawing;
 
 namespace BetterCMD
 {
     class BetterCMD
     {
-        static string promptFormat = Environment.GetEnvironmentVariable("CMDPrompt");
+        private static readonly string promptFormat = Environment.GetEnvironmentVariable("CMDPrompt", EnvironmentVariableTarget.User); // get the prompt
+
         static void Main(string[] args)
         {
-            if (promptFormat == null)
+            if (promptFormat == null) //prompt has not been configured
             {
                 Console.WriteLine("Configuring BetterCMD, Please Wait...");
-                Environment.SetEnvironmentVariable("CMDPrompt", "#14 BetterCMD #1 *10 nice~#1 test~~");
-                promptFormat = Environment.GetEnvironmentVariable("CMDPrompt");
+                Environment.SetEnvironmentVariable("CMDPrompt", "#14 BetterCMD #1 *10 nice~#1 test~~",
+                    EnvironmentVariableTarget.User); //configure prompt
+                Console.WriteLine("BetterCMD will work after you start it again!");
             }
-            Prompt prompt = new Prompt(promptFormat);
-            prompt.printPrompt();
+            else
+            {
+                
+                Prompt prompt = new Prompt("#14 BetterCMD #1 *10 nice~#1 test~~ {user}@{hostname}:{dirLin}>"); //create new prompt
+                prompt.printPrompt(); //print the prompt
+
+            }
         }
     }
+        
 }
--- BetterCMD/Prompt.cs
+++ BetterCMD/Prompt.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.ComponentModel.DataAnnotations;
 
 namespace BetterCMD
 {
@@ -13,42 +12,60 @@ namespace BetterCMD
 
         public void printPrompt ()
         {
-            /* example
-                test #14 yellow{hostname} *9 {user} \*>
-            */
             
-            //check for first special char
-            int nextFg = promptFormat.IndexOf("#");
-            int nextBg = promptFormat.IndexOf("*");
-            int nextSc = promptFormat.IndexOf("\\");
-            int nextSb = promptFormat.IndexOf("{");
-            int nextSe = promptFormat.IndexOf("}");
-            int[] special = new int[5] {nextFg, nextBg, nextSc, nextSb, nextSe};
-
+            //print the prompt
             for (int i = 0; i < promptFormat.Length; i++)
             {
-                if (promptFormat[i] == '#')
+                //check if char is a special char
+                if (promptFormat[i] == '#') //foreground color
                 {
-                    int nextWhiteSpace = promptFormat.IndexOf(" ", i);
-                    int colorCode = Int32.Parse(promptFormat.Substring(i+1, nextWhiteSpace - i));
-                    Console.ForegroundColor = (ConsoleColor)colorCode;
-                    i += promptFormat.Substring(i, nextWhiteSpace - i).Length;
-                } else if (promptFormat[i] == '*')
+                    int nextWhiteSpace = promptFormat.IndexOf(" ", i); //get next whitespace
+                    
+                    int colorCode = Int32.Parse(promptFormat.Substring(i+1, nextWhiteSpace - i)); //parse the color code to int
+                    Console.ForegroundColor = (ConsoleColor)colorCode; //convert it to Consoleolor
+                    
+                    i += promptFormat.Substring(i, nextWhiteSpace - i).Length; //skip the color code chars to prevent to be printed
+                    
+                } else if (promptFormat[i] == '*') // background color
                 {
-                    int nextWhiteSpace = promptFormat.IndexOf(" ", i);
-                    int colorCode = Int32.Parse(promptFormat.Substring(i+1, nextWhiteSpace - i));
-                    Console.BackgroundColor = (ConsoleColor)colorCode;
-                    i += promptFormat.Substring(i, nextWhiteSpace - i).Length;
+                    int nextWhiteSpace = promptFormat.IndexOf(" ", i); //get next whitespace
+                    
+                    int colorCode = Int32.Parse(promptFormat.Substring(i+1, nextWhiteSpace - i)); //parse the color code to int
+                    Console.BackgroundColor = (ConsoleColor)colorCode; //convert it to ConsoleColor
+                    
+                    i += promptFormat.Substring(i, nextWhiteSpace - i).Length; //skip the color code chars to prevent to be printed
+                    
                 } else if (promptFormat[i] == '~')
                 {
-                    Console.Write(promptFormat[++i]);
+                    Console.Write(promptFormat[++i]); //write the car after the ~ and skip 1 char
+                    
                 } else if (promptFormat[i] == '{')
                 {
+                    int closeBracket = promptFormat.IndexOf("}", i); //get next closed bracket
                     
+                    string varName = promptFormat.Substring(i + 1, closeBracket - i-1); // get the variable name
+                    
+                    //compare it
+                    if (varName == "user") //user
+                    {
+                        Console.Write(Environment.UserName);
+                    } else if (varName == "hostname") //hostname (lowercase)
+                    {
+                        Console.Write(Environment.MachineName.ToLower());
+                    } else if (varName == "dirWin") //current working dir in windows style
+                    {
+                        Console.Write(Environment.CurrentDirectory);
+                    } else if (varName == "dirLin") //current working dir in linux style
+                    {
+                        Console.Write(Environment.CurrentDirectory.Replace("\\","/"));
+                    }
+                    
+                    
+                    i += promptFormat.Substring(i, closeBracket - i).Length; //skip the brackets and variable name
                 }
                 else
                 {
-                    Console.Write(promptFormat[i]);
+                    Console.Write(promptFormat[i]); // nothing special just print the char
                 }
             }
         }
